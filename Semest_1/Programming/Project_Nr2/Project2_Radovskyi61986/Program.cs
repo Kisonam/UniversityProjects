d72bda1b6b1a850eda22126ea7b5199f42a08857
Nightmare St <107635756+Kisonam@users.noreply.github.com>
2022-11-22 22:00:23 +0200

    Update Program.cs

--- Semest_1/Programming/Project_Nr2/Project2_Radovskyi61986/Program.cs
+++ Semest_1/Programming/Project_Nr2/Project2_Radovskyi61986/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Project2_Radovskyi61986
+namespace Project2_####
 {
     internal static class Program
     {
