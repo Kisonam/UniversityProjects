bdb6f6af4523dc3dadcb3a6624001c06f88904b9
Nightmare St <107635756+Kisonam@users.noreply.github.com>
2022-11-23 23:01:29 +0100

    Add files via upload

---
+++ Semest_1/Programming/Project_Nr2/Project2_Radovskyi61986/Project2_Radovskyi61986.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project2_Radovskyi61986
+{
+    public partial class Project2_Radovskyi61986 : Form
+    {
+        public Project2_Radovskyi61986() // konstruktor klasy 
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Laboratoryjny
+        {
+            /* sprawdzenie i otszukanie w kolekcji OpenForms, czy zawiera on referencję
+               egzemplarza formularza: 'SzeregLaboratoryjny' */
+            foreach (Form Formularz in Application.OpenForms)
+            {// sprawdzenie, czy jest to formularz 'SzeregLaboratoryjny'
+                if(Formularz.Name == "SzeregLab")
+                {
+                    this.Hide(); //autorefrencja this "pokazuje" bieżący formularz
+                    //odsłonięcie formularza znalerzenego
+                    Formularz.Show();
+                    /* wyjcze z metody obsługi zdarzenia Click dla przycisku poleceń
+                       BtnLab*/
+                    return;
+                }
+                /* gdy jesteśmy "tutaj" to jest to "znak" że  egzemplara formularza 
+                 SzeregLaboratoryjny nie był jeście utworzony*/
+                SzeregLab AnalizatorSzeregu = new SzeregLab();
+                this.Hide();
+
+                AnalizatorSzeregu.Show();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Semest_1/Programming/Project_Nr2/Project2_Radovskyi61986/SzeregLab.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project2_Radovskyi61986
+{
+    public partial class SzeregLab : Form
+    {
+        public SzeregLab()
+        {
+            InitializeComponent();
+        }
+
+        private void SzeregLab_FormClosing(object sender, EventArgs e)
+        {
+            DialogResult OlnoMassage = MessageBox.Show("Czy rzechywicie chcesz zakąć ten " +
+                "formularz i powrócić do formularza głuwnego", this.Text,
+                MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button3);
+            // rozpoznanie decyzji użytkownika programu
+            if (OlnoMassage == DialogResult.Yes)
+            {
+                e.Cancel = true;
+
+                foreach (Form Formukaez in Application.OpenForms)
+                {
+                    if (Formukaez.Name == "Project2_Radovskyi61986")
+                    {
+                        this.Hide();
+
+                        Formukaez.Show();
+
+                        return;
+                    }
+                    Project2_Radovskyi61986 FormularzPr = new Project2_Radovskyi61986();
+                    this.Hide();
+                    FormularzPr.Show();
+                }
+            }
+            else
+                e.Cancle = false;
+        }
+    }
+}
