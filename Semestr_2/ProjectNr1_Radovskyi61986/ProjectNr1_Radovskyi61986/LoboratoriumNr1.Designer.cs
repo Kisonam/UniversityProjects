4a8edec91ddba76546f5f98ba44e9e2956da987e
Nightmare St <107635756+Kisonam@users.noreply.github.com>
2023-03-20 22:52:53 +0100

    Add files via upload

--- Semestr_2/ProjectNr1_Radovskyi61986/ProjectNr1_Radovskyi61986/LoboratoriumNr1.Designer.cs
+++ Semestr_2/ProjectNr1_Radovskyi61986/ProjectNr1_Radovskyi61986/LoboratoriumNr1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnWizualizacjaTabeleryczna = new System.Windows.Forms.Button();
             this.btnWizualizacjaGraficzna = new System.Windows.Forms.Button();
             this.btnAnimacja = new System.Windows.Forms.Button();
@@ -42,8 +43,16 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.koloryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.atrybutyLiniiTonuIObiektuAnimowanegoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kształtObiektuAnimowanegoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnWizualizacjaTabeleryczna
@@ -73,7 +82,7 @@
             this.btnAnimacja.Name = "btnAnimacja";
             this.btnAnimacja.Size = new System.Drawing.Size(263, 88);
             this.btnAnimacja.TabIndex = 2;
-            this.btnAnimacja.Text = "Animacja po linii tonu";
+            this.btnAnimacja.Text = "Animacja po linii toru\r\n(wyznaczonej przez \r\nszereg potęgowy)";
             this.btnAnimacja.UseVisualStyleBackColor = true;
             // 
             // btnResetuj
@@ -89,30 +98,30 @@
             // lblXd
             // 
             this.lblXd.AutoSize = true;
-            this.lblXd.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblXd.Location = new System.Drawing.Point(165, 46);
+            this.lblXd.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblXd.Location = new System.Drawing.Point(288, 46);
             this.lblXd.Name = "lblXd";
-            this.lblXd.Size = new System.Drawing.Size(33, 19);
+            this.lblXd.Size = new System.Drawing.Size(32, 19);
             this.lblXd.TabIndex = 4;
             this.lblXd.Text = "Xd:";
             // 
             // lblXg
             // 
             this.lblXg.AutoSize = true;
-            this.lblXg.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblXg.Location = new System.Drawing.Point(281, 48);
+            this.lblXg.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblXg.Location = new System.Drawing.Point(404, 48);
             this.lblXg.Name = "lblXg";
-            this.lblXg.Size = new System.Drawing.Size(33, 19);
+            this.lblXg.Size = new System.Drawing.Size(32, 19);
             this.lblXg.TabIndex = 5;
             this.lblXg.Text = "Xg:";
             // 
             // lblH
             // 
             this.lblH.AutoSize = true;
-            this.lblH.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblH.Location = new System.Drawing.Point(415, 46);
+            this.lblH.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblH.Location = new System.Drawing.Point(538, 46);
             this.lblH.Name = "lblH";
-            this.lblH.Size = new System.Drawing.Size(88, 57);
+            this.lblH.Size = new System.Drawing.Size(85, 57);
             this.lblH.TabIndex = 6;
             this.lblH.Text = "h:\r\n(krok zmian\r\n wartości)";
             // 
@@ -135,21 +144,21 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(204, 47);
+            this.textBox1.Location = new System.Drawing.Point(327, 47);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(71, 20);
             this.textBox1.TabIndex = 9;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(317, 47);
+            this.textBox2.Location = new System.Drawing.Point(440, 47);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(71, 20);
             this.textBox2.TabIndex = 10;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(509, 47);
+            this.textBox3.Location = new System.Drawing.Point(632, 47);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(79, 20);
             this.textBox3.TabIndex = 11;
@@ -157,11 +166,12 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(36, 51);
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 46);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(260, 19);
             this.label1.TabIndex = 12;
-            this.label1.Text = "label1";
+            this.label1.Text = "Przedział wartości zmiennej niezależnej X:";
             // 
             // pictureBox1
             // 
@@ -170,12 +180,62 @@
             this.pictureBox1.Size = new System.Drawing.Size(924, 459);
             this.pictureBox1.TabIndex = 13;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem,
+            this.koloryToolStripMenuItem,
+            this.atrybutyLiniiTonuIObiektuAnimowanegoToolStripMenuItem,
+            this.kształtObiektuAnimowanegoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1244, 24);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.plikToolStripMenuItem.Text = "Plik";
+            // 
+            // koloryToolStripMenuItem
+            // 
+            this.koloryToolStripMenuItem.Name = "koloryToolStripMenuItem";
+            this.koloryToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.koloryToolStripMenuItem.Text = "Kolory";
+            // 
+            // atrybutyLiniiTonuIObiektuAnimowanegoToolStripMenuItem
+            // 
+            this.atrybutyLiniiTonuIObiektuAnimowanegoToolStripMenuItem.Name = "atrybutyLiniiTonuIObiektuAnimowanegoToolStripMenuItem";
+            this.atrybutyLiniiTonuIObiektuAnimowanegoToolStripMenuItem.Size = new System.Drawing.Size(243, 20);
+            this.atrybutyLiniiTonuIObiektuAnimowanegoToolStripMenuItem.Text = "Atrybuty linii tonu i obiektu animowanego";
+            // 
+            // kształtObiektuAnimowanegoToolStripMenuItem
+            // 
+            this.kształtObiektuAnimowanegoToolStripMenuItem.Name = "kształtObiektuAnimowanegoToolStripMenuItem";
+            this.kształtObiektuAnimowanegoToolStripMenuItem.Size = new System.Drawing.Size(175, 20);
+            this.kształtObiektuAnimowanegoToolStripMenuItem.Text = "Kształt obiektu animowanego";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(603, 310);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(8, 20);
+            this.dateTimePicker1.TabIndex = 15;
             // 
             // LoboratoriumNr1_Raovskyi61986
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1244, 629);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox3);
@@ -190,12 +250,16 @@
             this.Controls.Add(this.btnAnimacja);
             this.Controls.Add(this.btnWizualizacjaGraficzna);
             this.Controls.Add(this.btnWizualizacjaTabeleryczna);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "LoboratoriumNr1_Raovskyi61986";
             this.Text = "LoboratoriumNr1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.LoboratoriumNr1_Raovskyi61986_FormClosing);
             this.Load += new System.EventHandler(this.LoboratoriumNr1_Raovskyi61986_Load);
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -217,5 +281,12 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem koloryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem atrybutyLiniiTonuIObiektuAnimowanegoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kształtObiektuAnimowanegoToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
--- Semestr_2/ProjectNr1_Radovskyi61986/ProjectNr1_Radovskyi61986/LoboratoriumNr1.cs
+++ Semestr_2/ProjectNr1_Radovskyi61986/ProjectNr1_Radovskyi61986/LoboratoriumNr1.cs
@@ -12,9 +12,29 @@ namespace ProjectNr1_Radovskyi61986
 {
     public partial class LoboratoriumNr1_Raovskyi61986 : Form
     {
+        const int Margines = 10;
+        const int PromienObiekty = 5;
+        // deklaracje pomocnicza
+        static LoboratoriumNr1_Raovskyi61986 UchwytFormularza;
+        float Xd, Xg;
+        //
+        float[,] TWS = null;
+        int IndexPOA;
+        int MaxIndexPOA;
+        Graphics Rysownica;
+        Pen PióroXY;
+        Pen PioroLiniiToru;
+
         public LoboratoriumNr1_Raovskyi61986()
         {
             InitializeComponent();
+            UchwytFormularza = this;
+            pictureBox1.BackColor = Color.LightBlue;
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox1.Image = new Bitmap (pictureBox1.Width, pictureBox1.Height);
+
+            PióroXY = new Pen(Color.Blue, 1);
+            PioroLiniiToru = new Pen(Color.Black, 1.5f);
         }
 
         private void LoboratoriumNr1_Raovskyi61986_Load(object sender, EventArgs e)
@@ -38,6 +58,75 @@ namespace ProjectNr1_Radovskyi61986
             Kokpit_Radovskyi61986 Laboratorium = new Kokpit_Radovskyi61986();
             Laboratorium.Show();
             this.Hide();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (IndexPOA >= MaxIndexPOA)
+                IndexPOA = 0;
+            else
+                IndexPOA++;  
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Rysownica.Clear(Color.AliceBlue);
+            Rysownica.DrawLine(PióroXY, PrzeliczanieWsprznych.WspX(0),
+                PrzeliczanieWsprznych.WspY(PrzeliczanieWsprznych.Ymax),
+                PrzeliczanieWsprznych.WspX(0), PrzeliczanieWsprznych.WspY(PrzeliczanieWsprznych.Ymin));
+            Rysownica.DrawLine(PióroXY, PrzeliczanieWsprznych.WspX(PrzeliczanieWsprznych.Xmin),
+               PrzeliczanieWsprznych.WspY(0), PrzeliczanieWsprznych.WspY(PrzeliczanieWsprznych.Xmax), PrzeliczanieWsprznych.WspY(0));
+
+            for (int j = 0; j < TWS.GetLength(0) - 1; j++)
+            {
+                Rysownica.DrawLine(PioroLiniiToru, PrzeliczanieWsprznych.WspX(TWS[j, 0]),
+                   PrzeliczanieWsprznych.WspY(TWS[j, 1]),
+                   PrzeliczanieWsprznych.WspX(TWS[j + 1, 0]),
+                   PrzeliczanieWsprznych.WspX(TWS[j + 1, 1]));
+            }
+            Rysownica.FillEllipse(Brushes.Yellow, PrzeliczanieWsprznych.WspX(TWS[IndexPOA, 0]) - PromienObiekty, PrzeliczanieWsprznych.WspX(TWS[IndexPOA, 1]) - PromienObiekty, 2 * PromienObiekty, PromienObiekty);
+        }
+
+        public static class PrzeliczanieWsprznych 
+        {
+            static float Sx, Sy;
+            static float Dx, Dy;
+            // deklaracje zmiannych dla przechowania rozmiarów powierzchni graficznej
+            static int Xe_min, Xe_max, Ye_min, Ye_max;
+            public static float Xmax { get;private set; }
+            public static float Xmin { get; private set; }
+            public static float Ymin { get; private set; }
+            public static float Ymax { get; private set; }
+
+            static PrzeliczanieWsprznych()
+            {
+                Xmin = UchwytFormularza.Xd;
+                Xmax = UchwytFormularza.Xg;
+                //
+                Ymin = ExtremuSzeregu.MinSx(UchwytFormularza.TWS);
+                Ymax = ExtremuSzeregu.MaxSx(UchwytFormularza.TWS);
+                //
+                Xe_min = Margines;
+                Xe_max = UchwytFormularza.pictureBox1.Width - (Margines + Margines);
+                Ye_min = Margines;
+                Ye_max = UchwytFormularza.pictureBox1.Height - (Margines + Margines);
+                //
+                Sx = (Xe_max - Xe_min) / (Xmax- Xmin);
+                Sy = (Ye_max - Ye_min) / (Ymax- Ymin);
+                Dx = Xe_min - Xmin * Sx;
+                Dy = Ye_min - Ymin * Sx;
+
+            }
+            // deklaracja metod
+            public static int WspX(float x)
+            {
+                return (int)(Sx * x + Dx);
+            }
+            public static int WspY(float y)
+            {
+                return (int)((Sx * y) + Dy);
+            }
         }
     }
 }
